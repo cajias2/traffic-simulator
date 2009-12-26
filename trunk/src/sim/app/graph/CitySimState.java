e64d3cafab99db39fd19359ea3ad667a7dcd7294
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2009-12-26 13:37:42 +0000

    Accomplishments:  - Traffic Light Created  - Xings can have traffic lights  - Cars know the state of the traffic light
    Not working:
     - Cars dont stop on red.

--- trunk/src/sim/app/graph/CitySimState.java
+++ trunk/src/sim/app/graph/CitySimState.java
@@ -14,15 +14,27 @@ import sim.engine.SimState;
 public class CitySimState extends SimState {
 
 	private Graph<StreetXing,Street> _city;
-
+	
+	/**
+	 * 
+	 * @param seed
+	 */
 	public CitySimState(long seed) {
 		super(seed);
 	}
-
+	
+	/**
+	 * 
+	 * @param city_
+	 */
 	public void setCity(Graph<StreetXing,Street> city_) {
 		_city = city_;
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public Graph<StreetXing,Street> getCity()
 	{
 		return _city;
