db65dfc2ceb10a4eda50f2e546137c2a249176db
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2010-03-12 10:59:20 +0000

    Roads made up of individual lines work

--- src/sim/app/geo/Road.java
+++ src/sim/app/geo/Road.java
@@ -21,275 +21,284 @@ import sim.app.geo.distance.Meters;
  * 
  */
 public abstract class Road {
-    private static final Distance LAYER_SEG = new Meters(100);
+	private static final Distance LAYER_SEG = new Meters(100);
 
-    private static final double DISTANCE_THRESHOLD = 1;
+	private static final double DISTANCE_THRESHOLD = 0.001;
 
-    public final String ID;
+	public final String ID;
 
-    private static PApplet _parent;
+	private static PApplet _parent;
 
-    private List<Line2D> _lineList;
-    private double _realLength = 0;
-    private List<Double> _velocityLayer;
-    private LinkedList<List<Line2D>> _segmentList;
-    private List<Vehicle> _vehiclesOnRoad;
+	private List<Line2D> _lineList;
+	private double _realLength = 0;
+	private List<Double> _velocityLayer;
+	private LinkedList<List<Line2D>> _segmentList;
+	private List<Vehicle> _vehiclesOnRoad;
 
-    private Distance _geoLength;
+	private Distance _geoLength;
 
-    public abstract Distance getMaxVelocity();
+	public abstract Distance getMaxVelocity();
 
-    protected abstract int getStrokeWeight();
+	protected abstract int getStrokeWeight();
 
-    /**
-     * Constructor
-     */
-    public Road(String id_, List<Point2D> pointList_, PApplet parent_) {
-	if (_parent == null) {
-	    _parent = parent_;
-	}
-	ID = id_;
-	_vehiclesOnRoad = new LinkedList<Vehicle>();
-	createRoad(pointList_);
-	// Use total street length to determine layer size.
-	_velocityLayer = new ArrayList<Double>((int) Math.ceil(_realLength
-		/ LAYER_SEG.getVal()));
-	processRoadSegments();
-    }
-
-    /**
-     * Create a a list of line segments for disaq1play
-     */
-    private void processRoadSegments() {
-	_segmentList = new LinkedList<List<Line2D>>();
-
-	for (Line2D currLine : _lineList) {
-	    double lineLength = currLine.getP1().distance(currLine.getP2());
-
-	    // Divide the line into segments and store them in a map
-	    _segmentList.add(new LinkedList<Line2D>());
-	    int totalSegments = (int) Math
-		    .ceil(lineLength / LAYER_SEG.getVal());
-	    findRoadSegment(currLine, totalSegments);
-	}
-    }
-
-    /**
-     * Helper method that breaks a line into smaller subsegments.
-     * 
-     * @param line_
-     * @param totalSegments_
-     * @return
-     */
-    private void findRoadSegment(Line2D line_, int totalSegments_) {
-
-	// Select direction to move in.
-	boolean dir = true;
-	if (line_.getX1() > line_.getX2())
-	    dir = false;
-	Point2D segOrig = null;
-	Point2D segDest = null;
-
-	for (int i = 0; i < totalSegments_; i++) {
-	    if (segOrig == null) { // If this is the first round...
-		segOrig = line_.getP1();
-	    } else {
-		segOrig = segDest;
-	    }
-	    if ((i + 1) >= totalSegments_) { // Last Round
-		segDest = line_.getP2();
-	    } else {
-		segDest = getNewLocation(segOrig, line_, LAYER_SEG.getVal(), dir);
-	    }
-	    _segmentList.getLast().add(new Line2D.Double(segOrig, segDest));
-	}
-    }
-
-    /**
-     * Determines next location, based on current location and how much to move.
-     * Not as efficient as overloaded version where the current line is also 
-     * known.  
-     * @param curLoc_
-     * @param len_
-     * @param dir_
-     * @return
-     */
-    public Point2D getNewLocation(Point2D curLoc_, double len_, boolean dir_) {
-	Line2D line = getLine(curLoc_);
-	return getNewLocation(curLoc_, line, len_, dir_);
-    }
-    private Line2D getLine(Point2D curLoc) {
-	Line2D currLine = null;
-	
-	for(Line2D line : _lineList)
-	{
-	    if(line.ptLineDist(curLoc) < DISTANCE_THRESHOLD){
-		currLine = line;
-		break;
-	    }
-	}
-	
-	return currLine;
-    }
-
-    /**
-     * Given a line, find a secant line that intersects it at length len_ 
-     * 
-     * @param curLoc_
-     * @param len_
-     * @param dir_
-     * @return
-     */
-    public Point2D getNewLocation(Point2D curLoc_, Line2D line_, double len_, boolean dir_) {
-	Point2D newLoc_ = null;
-	
-	double slope = (line_.getY2() - line_.getY1())
-		/ (line_.getX2() - line_.getX1());
-	double slopeDeg = Math.atan(slope);
-	double runLen = Math.abs(Math.cos(slopeDeg) *len_);
-	
-	int direction = 1;
-	if (!dir_) {
-	    direction = -1;
-	}
-	Point2D secantOrig = new Point2D.Double(curLoc_.getX() + direction
-		* runLen, curLoc_.getY());
-	Point2D secantDest = new Point2D.Double(secantOrig.getX(),
-		secantOrig.getY() + 1);	
-	Line2D secant = new Line2D.Double(secantOrig, secantDest);
-	newLoc_ = intersectionPoint(line_, secant);
-
-	if (newLoc_ == null)
-	    throw new RuntimeException(
-		    "Intersection should not be null");
-	return newLoc_;
-    }    
-
-    /**
-     * @param pointList_
-     */
-    private void createRoad(List<Point2D> pointList_) {
-	_lineList = new LinkedList<Line2D>();
-	/*
-	 * Create the segments based on point list.
+	/**
+	 * Constructor
 	 */
-	Point2D orig = null, dest = null;
-	for (Point2D current : pointList_) {
-	    dest = current;
-	    if (orig == null) { // handle base case
-		orig = current;
-	    } else {
-		_lineList.add(new Line2D.Float(orig, dest));
-		_realLength += orig.distance(dest);
-		orig = dest;
-	    }
+	public Road(String id_, List<Point2D> pointList_, PApplet parent_) {
+		if (_parent == null) {
+			_parent = parent_;
+		}
+		ID = id_;
+		_vehiclesOnRoad = new LinkedList<Vehicle>();
+		createRoad(pointList_);
+		// Use total street length to determine layer size.
+		_velocityLayer = new ArrayList<Double>((int) Math.ceil(_realLength
+				/ LAYER_SEG.getVal()));
+		processRoadSegments();
 	}
-	_geoLength = new Kilometers(_realLength);
-	System.out.println("Pixel Lenght:" + _realLength);
-	System.out.println("Scale Length: " + _geoLength);
-    }
 
-    /**
-     * @return the _roadLength
-     */
-    public double getRoadLength() {
-	return _realLength;
-    }
-
-    /**
-     * Display the road.
-     */
-    public void display() {
-	_parent.noFill();
-	_parent.smooth();
-
-	/*
-	 * Display the road by going though each line in the list that comprises
-	 * it and displaying it individually.
+	/**
+	 * Create a a list of line segments for disaq1play
 	 */
-	for (Line2D currSeg : _lineList) {
-	    _parent.stroke(0);
-	    _parent.strokeWeight(getStrokeWeight());
-	    _parent.line((float) currSeg.getP1().getX(), (float) currSeg
-		    .getP1().getY(), (float) currSeg.getP2().getX(),
-		    (float) currSeg.getP2().getY());
+	private void processRoadSegments() {
+		_segmentList = new LinkedList<List<Line2D>>();
+
+		for (Line2D currLine : _lineList) {
+			double lineLength = currLine.getP1().distance(currLine.getP2());
+
+			// Divide the line into segments and store them in a map
+			_segmentList.add(new LinkedList<Line2D>());
+			int totalSegments = (int) Math
+					.ceil(lineLength / LAYER_SEG.getVal());
+			findRoadSegment(currLine, totalSegments);
+		}
 	}
-	/*
-	 * Paint each line from _segment list on top of the street we just
-	 * painted. These segments will vary in color/weight depending on the
-	 * cars cruising it.
+
+	/**
+	 * Helper method that breaks a line into smaller subsegments.
+	 * 
+	 * @param line_
+	 * @param totalSegments_
+	 * @return
 	 */
-	// TODO
-	// for(Vehicle vhcl: _vehiclesOnRoad)
-	// {
-	// if(vhcl.)
-	// }
-	for (List<Line2D> line : _segmentList) {
-	    Iterator<Line2D> iter = line.iterator();
-	    float color = 1;
+	private void findRoadSegment(Line2D line_, int totalSegments_) {
 
-	    while (iter.hasNext()) {
-		Line2D seg = (Line2D) iter.next();
-		if (color >= 10)
-		    color = 1;
-		_parent.stroke(255);
-		_parent.strokeWeight(color);
-		_parent.point((float) seg.getX1(), (float) seg.getY1());
-		_parent.point((float) seg.getX2(), (float) seg.getY2());
-		color += 0.1;
-	    }
+
+
+		Point2D segOrig = null;
+		Point2D segDest = null;
+
+		for (int i = 0; i < totalSegments_; i++) {
+			if (segOrig == null) { // If this is the first round...
+				segOrig = line_.getP1();
+			} else {
+				segOrig = segDest;
+			}
+			if ((i + 1) >= totalSegments_) { // Last Round
+				segDest = line_.getP2();
+			} else {
+				segDest = getNewLocation(segOrig, line_, LAYER_SEG.getVal());
+			}
+			_segmentList.getLast().add(new Line2D.Double(segOrig, segDest));
+		}
 	}
-    }
 
-    /**
-     * TODO javadoc
-     * 
-     * @param line1_
-     * @param line2_
-     * @return
-     */
-    private static Point2D intersectionPoint(Line2D line1_, Line2D line2_) {
-	Point2D p = null;
-	double x1 = line1_.getX1();
-	double y1 = line1_.getY1();
-	double x2 = line1_.getX2();
-	double y2 = line1_.getY2();
+	/**
+	 * Determines next location, based on current location and how much to move.
+	 * Not as efficient as overloaded version where the current line is also
+	 * known.
+	 * 
+	 * @param curLoc_
+	 * @param len_
+	 * @param dir_
+	 * @return
+	 */
+	public Point2D getNewLocation(Point2D curLoc_, double len_) {
+		Line2D line = getLine(curLoc_);
+		return getNewLocation(curLoc_, line, len_);
+	}
 
-	double x3 = line2_.getX1();
-	double y3 = line2_.getY1();
-	double x4 = line2_.getX2();
-	double y4 = line2_.getY2();
+	private Line2D getLine(Point2D curLoc) {
+		Line2D currLine = null;
 
-	double det = (x1 - x2) * (y3 - y4) - (y1 * y2) * (x3 - x4);
-	double px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2)
-		* (x3 * y4 - y3 * x4))
-		/ det;
-	double py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2)
-		* (x3 * y4 - y3 * x4))
-		/ det;
-	p = new Point2D.Double(px, py);
-	return p;
-    }
+		for (Line2D line : _lineList) {
+			if (line.ptLineDist(curLoc) < DISTANCE_THRESHOLD) {
+				currLine = line;
+				break;
+			}
+		}
 
-    public Point2D startLoc() {
-	return _lineList.get(0).getP1();
-    }
+		return currLine;
+	}
 
-    public Point2D endLoc() {
-	return _lineList.get(_lineList.size() - 1).getP2();
-    }
+	/**
+	 * Given a line, find a secant line that intersects it at length len_
+	 * 
+	 * @param curLoc_
+	 * @param len_
+	 * @param dir_
+	 * @return
+	 */
+	public Point2D getNewLocation(Point2D curLoc_, Line2D line_, double len_) {
+		// Select direction to move in.
+		int[] direction = { 1, 1 };
+		if (line_.getX1() > line_.getX2())
+			direction[0] = -direction[0];
+		if (line_.getY1() > line_.getY2())
+			direction[1] = -direction[0];
+		
+		Point2D newLoc_ = null;
 
-    /**
-     * @return the _carsOnRoad
-     */
-    public List<Vehicle> getVehiclesOnRoad() {
-	return _vehiclesOnRoad;
-    }
+		double slope = (line_.getY2() - line_.getY1())
+				/ (line_.getX2() - line_.getX1());
+		double slopeDeg = Math.atan(slope);
+		double runLen = Math.abs(Math.cos(slopeDeg) * len_);
 
-    /**
-     * Class toString
-     */
-    public String toString() {
-	return ID + "__" + _vehiclesOnRoad.size();
-    }
+		Point2D secantOrig = new Point2D.Double(curLoc_.getX() + direction[0]
+				* runLen, curLoc_.getY());
+		Point2D secantDest = new Point2D.Double(secantOrig.getX(), secantOrig
+				.getY() + 1);
+		Line2D secant = new Line2D.Double(secantOrig, secantDest);
+		newLoc_ = intersectionPoint(line_, secant);
+		
+		// Am I over the boundary  of the line?
+		if(curLoc_.distance(line_.getP2()) < curLoc_.distance(newLoc_))
+		{
+//			List<Line2D> subList = _lineList.subList(_lineList.indexOf(line_), _lineList.size());
+			Line2D nextLine = _lineList.get(_lineList.indexOf(line_) + 1);
+			newLoc_ = getNewLocation(nextLine.getP1(), nextLine, len_ - (curLoc_.distance(nextLine.getP1())));
+		}
+
+		if (newLoc_ == null)
+			throw new RuntimeException("Intersection should not be null");
+		return newLoc_;
+	}
+
+	/**
+	 * @param pointList_
+	 */
+	private void createRoad(List<Point2D> pointList_) {
+		_lineList = new LinkedList<Line2D>();
+		/*
+		 * Create the segments based on point list.
+		 */
+		Point2D orig = null, dest = null;
+		for (Point2D current : pointList_) {
+			dest = current;
+			if (orig == null) { // handle base case
+				orig = current;
+			} else {
+				_lineList.add(new Line2D.Float(orig, dest));
+				_realLength += orig.distance(dest);
+				orig = dest;
+			}
+		}
+		_geoLength = new Kilometers(_realLength);
+		System.out.println("Pixel Lenght:" + _realLength);
+		System.out.println("Scale Length: " + _geoLength);
+	}
+
+	/**
+	 * @return the _roadLength
+	 */
+	public double getRoadLength() {
+		return _realLength;
+	}
+
+	/**
+	 * Display the road.
+	 */
+	public void display() {
+		_parent.noFill();
+		_parent.smooth();
+
+		/*
+		 * Display the road by going though each line in the list that comprises
+		 * it and displaying it individually.
+		 */
+		for (Line2D currSeg : _lineList) {
+			_parent.stroke(0);
+			_parent.strokeWeight(getStrokeWeight());
+			_parent.line((float) currSeg.getP1().getX(), (float) currSeg
+					.getP1().getY(), (float) currSeg.getP2().getX(),
+					(float) currSeg.getP2().getY());
+		}
+		/*
+		 * Paint each line from _segment list on top of the street we just
+		 * painted. These segments will vary in color/weight depending on the
+		 * cars cruising it.
+		 */
+		// TODO
+		// for(Vehicle vhcl: _vehiclesOnRoad)
+		// {
+		// if(vhcl.)
+		// }
+		for (List<Line2D> line : _segmentList) {
+			Iterator<Line2D> iter = line.iterator();
+			float color = 1;
+
+			while (iter.hasNext()) {
+				Line2D seg = (Line2D) iter.next();
+				if (color >= 10)
+					color = 1;
+				_parent.stroke(255);
+				_parent.strokeWeight(color);
+				_parent.point((float) seg.getX1(), (float) seg.getY1());
+				_parent.point((float) seg.getX2(), (float) seg.getY2());
+				color += 0.1;
+			}
+		}
+	}
+
+	/**
+	 * TODO javadoc
+	 * 
+	 * @param line1_
+	 * @param line2_
+	 * @return
+	 */
+	private static Point2D intersectionPoint(Line2D line1_, Line2D line2_) {
+		Point2D p = null;
+		double x1 = line1_.getX1();
+		double y1 = line1_.getY1();
+		double x2 = line1_.getX2();
+		double y2 = line1_.getY2();
+
+		double x3 = line2_.getX1();
+		double y3 = line2_.getY1();
+		double x4 = line2_.getX2();
+		double y4 = line2_.getY2();
+
+		double det = (x1 - x2) * (y3 - y4) - (y1 * y2) * (x3 - x4);
+		double px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2)
+				* (x3 * y4 - y3 * x4))
+				/ det;
+		double py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2)
+				* (x3 * y4 - y3 * x4))
+				/ det;
+		p = new Point2D.Double(px, py);
+		return p;
+	}
+
+	public Point2D startLoc() {
+		return _lineList.get(0).getP1();
+	}
+
+	public Point2D endLoc() {
+		return _lineList.get(_lineList.size() - 1).getP2();
+	}
+
+	/**
+	 * @return the _carsOnRoad
+	 */
+	public List<Vehicle> getVehiclesOnRoad() {
+		return _vehiclesOnRoad;
+	}
+
+	/**
+	 * Class toString
+	 */
+	public String toString() {
+		return ID + "__" + _vehiclesOnRoad.size();
+	}
 }
--- src/sim/app/processing/TrafficSim.java
+++ src/sim/app/processing/TrafficSim.java
@@ -1,195 +1,187 @@
 package sim.app.processing;
 
-import java.awt.geom.Point2D;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
-import edu.uci.ics.jung.graph.Graph;
 
 import processing.core.PApplet;
 import sim.app.agents.Car;
 import sim.app.agents.Vehicle;
 import sim.app.geo.Road;
-import sim.app.geo.Street;
 import sim.app.geo.StreetXing;
 import sim.app.xml.XmlParseService;
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
+import edu.uci.ics.jung.graph.Graph;
 
 @SuppressWarnings("serial")
 public class TrafficSim extends PApplet {
-    private static final int FRAME_RATE = 30;
-    private static final double NEW_VHCL_RATIO = .1;
-    private static Graph<StreetXing, Road> _city;
-    private static Logger _log;
-    private static String clazz = TrafficSim.class.getSimpleName();
-    private static String _cityXml = "/../src/xml/OneStreet.xml";
-    private List<StreetXing> _sourceXings;
-    private List<StreetXing> _destXings;
+	private static final int FRAME_RATE = 30;
+	private static final double NEW_VHCL_RATIO = .1;
+	private static Graph<StreetXing, Road> _city;
+	private static Logger _log;
+	private static String clazz = TrafficSim.class.getSimpleName();
+	private static String _cityXml = "/../src/xml/OneStreet.xml";
+	private List<StreetXing> _sourceXings;
+	private List<StreetXing> _destXings;
 
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
-    public static int MAX_CAR_COUNT = 3;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	public static int MAX_CAR_COUNT = 3;
 
-    private static LinkedList<Vehicle> _vehicleAgents;
-    private static LinkedList<Road> _roads;
+	private static LinkedList<Vehicle> _vehicleAgents;
+	private static LinkedList<Road> _roads;
 
-    /**
-     * Creates a TrafficSim simulation with the given random number seed.
-     */
-    // public TrafficSim(long seed, String cityXmlFileName_, Logger log_)
-    // {
-    // super( seed );
-    // XmlParseService parsedGraph = new XmlParseService( cityXmlFileName_, log_
-    // );
-    // setCity( parsedGraph.getGraph() );
-    // MAX_CAR_COUNT = parsedGraph.getMaxCars();
-    // _sourceXings = parsedGraph.getSourceXings();
-    // _destXings = parsedGraph.getDestXings();
-    // _log = log_;
-    // }
-
-    // *************
-
-    public void setup() {
-	size(WIDTH, HEIGHT);
-	frameRate(FRAME_RATE);
-	_vehicleAgents = new LinkedList<Vehicle>();
-	// Create first batch of cars
-	parseGraph();
-	for (int i = 0; i < 1; i++) {
-	    _vehicleAgents.add(generateVehicle());
-	}
-
-	_roads = new LinkedList<Road>(_city.getEdges());
-
-    }
-
-    private void parseGraph() {
-	XmlParseService parsedGraph = new XmlParseService(System.getProperty("user.dir")+_cityXml, this,_log);
-	_city = parsedGraph.getGraph();
-	MAX_CAR_COUNT = parsedGraph.getMaxCars();
-	_sourceXings = parsedGraph.getSourceXings();
-	_destXings = parsedGraph.getDestXings();
-
-    }
-    
-    /**
-     * TODO expand for all kinds of vehicles.. right now hardcoded to cars :(
-     * 
-     * @return
-     */
-    private Vehicle generateVehicle()
-    {
-	Vehicle vcle;
-        DijkstraShortestPath<StreetXing, Road> routeMap = new DijkstraShortestPath<StreetXing, Road>( _city );
-        StreetXing source = getSource();
-        StreetXing target = getTarget();
-        // Make sure start and end are different. Otherwise...
-        // what's the point?
-        // Fixed nullpointer exception by checking target=?null.
-        while (( target == null || target.getId() ==  null) ||
-                source.getId().equals( target.getId() ) )
-        {
-            target = getTarget();
-        }
-        List<Road> trayectory = routeMap.getPath( source, target );
-        vcle = new Car( trayectory, _city, _log, this );
-        return vcle;
-    }
-
-    public void draw() {
-	background(100);
-	// Move and display all "stripes"
-	
-	/*
-	 * Display roads
+	// *************
+	/**
+	 * 
 	 */
-	for(Road rd: _roads)
-	{
-	    rd.display();
+	public void setup() {
+		size(WIDTH, HEIGHT);
+		frameRate(FRAME_RATE);
+		_vehicleAgents = new LinkedList<Vehicle>();
+		// Create first batch of cars
+		parseGraph();
+		for (int i = 0; i < 1; i++) {
+			_vehicleAgents.add(generateVehicle());
+		}
+
+		_roads = new LinkedList<Road>(_city.getEdges());
+
 	}
-	/*
-	 * Display cars
+
+	/**
+	 * 
 	 */
-	Iterator<Vehicle> iter = _vehicleAgents.iterator();
-	while (iter.hasNext()) {
-	    Vehicle vhcl = iter.next();
-	    if (vhcl.isAlive()) {
-		vhcl.move();
-		vhcl.display();
-	    } else {
-		iter.remove();
-	    }
+	private void parseGraph() {
+		XmlParseService parsedGraph = new XmlParseService(System
+				.getProperty("user.dir")
+				+ _cityXml, this, _log);
+		_city = parsedGraph.getGraph();
+		MAX_CAR_COUNT = parsedGraph.getMaxCars();
+		_sourceXings = parsedGraph.getSourceXings();
+		_destXings = parsedGraph.getDestXings();
+
 	}
-	/*
-	 * Add new cars to the mix?
+
+	/**
+	 * TODO expand for all kinds of vehicles.. right now hardcoded to cars :(
+	 * 
+	 * @return
 	 */
-	if(addVhclP()){
-	    _vehicleAgents.add(generateVehicle());
-	}
-    }
-
-    // ****************
-    private StreetXing getTarget() {
-	StreetXing pickedXing = null;
-	Random rand = new Random(System.currentTimeMillis());
-	int targetIndex = rand.nextInt(100);
-	int currentIndex = 0;
-
-	for (StreetXing xing : _sourceXings) {
-	    currentIndex += xing.getStartOdds();
-	    if (currentIndex >= targetIndex) {
-		pickedXing = xing;
-		break;
-	    }
-	}
-	return pickedXing;
-    }
-
-    /**
-     * Return a random car begining based on the {@code startingOdds} attribute
-     * in the city xml.
-     * <p/>
-     * See {@link TraffiSimulation.xsd} for more info
-     * 
-     * @return
-     */
-    private StreetXing getSource() {
-	StreetXing pickedXing = null;
-	Random rand = new Random(System.currentTimeMillis());
-	int targetIndex = rand.nextInt(100);
-	int currentIndex = 0;
-
-	for (StreetXing xing : _destXings) {
-	    currentIndex += xing.getEndOdds();
-	    if (currentIndex >= targetIndex) {
-		pickedXing = xing;
-		break;
-	    }
+	private Vehicle generateVehicle() {
+		Vehicle vcle;
+		DijkstraShortestPath<StreetXing, Road> routeMap = new DijkstraShortestPath<StreetXing, Road>(
+				_city);
+		StreetXing source = getSource();
+		StreetXing target = getTarget();
+		// Make sure start and end are different. Otherwise...
+		// what's the point?
+		// Fixed nullpointer exception by checking target=?null.
+		while ((target == null || target.getId() == null)
+				|| source.getId().equals(target.getId())) {
+			target = getTarget();
+		}
+		List<Road> trayectory = routeMap.getPath(source, target);
+		vcle = new Car(trayectory, _city, _log, this);
+		return vcle;
 	}
 
-	return pickedXing;
-    }
+	/**
+	 * 
+	 */
+	public void draw() {
+		background(100);
+		// Move and display all "stripes"
 
-    /**
-     * Return number of cars to generate per step... Should follow a sinoidal
-     * function to mimic traffic waves.
-     * 
-     * @return cars to generate per step
-     */
-    private boolean addVhclP() {
-	if (Math.random() < NEW_VHCL_RATIO)
-	    return true;
-	return false;	    	    
-//	double carPerStep = Math.floor(Math
-//		.sin((System.currentTimeMillis() + 473) / 100)
-//		* (MAX_CAR_COUNT / 2) + MAX_CAR_COUNT / 2);
-	// _log.log( Level.FINE, "Will try to create: "+carPerStep+"cars" );
-//	return carPerStep;
+		/*
+		 * Display roads
+		 */
+		for (Road rd : _roads) {
+			rd.display();
+		}
+		/*
+		 * Display cars
+		 */
+		Iterator<Vehicle> iter = _vehicleAgents.iterator();
+		while (iter.hasNext()) {
+			Vehicle vhcl = iter.next();
+			if (vhcl.isAlive()) {
+				vhcl.move();
+				vhcl.display();
+			} else {
+				iter.remove();
+			}
+		}
+		/*
+		 * Add new cars to the mix?
+		 */
+		if (addVhclP()) {
+			_vehicleAgents.add(generateVehicle());
+		}
+	}
 
-    }
+	/**
+	 * 
+	 * @return
+	 */
+	private StreetXing getTarget() {
+		StreetXing pickedXing = null;
+		Random rand = new Random(System.currentTimeMillis());
+		int targetIndex = rand.nextInt(100);
+		int currentIndex = 0;
+
+		for (StreetXing xing : _sourceXings) {
+			currentIndex += xing.getStartOdds();
+			if (currentIndex >= targetIndex) {
+				pickedXing = xing;
+				break;
+			}
+		}
+		return pickedXing;
+	}
+
+	/**
+	 * Return a random car begining based on the {@code startingOdds} attribute
+	 * in the city xml.
+	 * <p/>
+	 * See {@link TraffiSimulation.xsd} for more info
+	 * 
+	 * @return
+	 */
+	private StreetXing getSource() {
+		StreetXing pickedXing = null;
+		Random rand = new Random(System.currentTimeMillis());
+		int targetIndex = rand.nextInt(100);
+		int currentIndex = 0;
+
+		for (StreetXing xing : _destXings) {
+			currentIndex += xing.getEndOdds();
+			if (currentIndex >= targetIndex) {
+				pickedXing = xing;
+				break;
+			}
+		}
+
+		return pickedXing;
+	}
+
+	/**
+	 * Return number of cars to generate per step... Should follow a sinoidal
+	 * function to mimic traffic waves.
+	 * 
+	 * @return cars to generate per step
+	 */
+	private boolean addVhclP() {
+		if (Math.random() < NEW_VHCL_RATIO)
+			return true;
+		return false;
+		// double carPerStep = Math.floor(Math
+		// .sin((System.currentTimeMillis() + 473) / 100)
+		// * (MAX_CAR_COUNT / 2) + MAX_CAR_COUNT / 2);
+		// _log.log( Level.FINE, "Will try to create: "+carPerStep+"cars" );
+		// return carPerStep;
+
+	}
 }
