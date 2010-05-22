2f16694317de67b8384a3820cb622354d97484de
antonio.gonz.par <antonio.gonz.par@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2010-05-22 12:20:35 +0000

    Estas son mis dos modestas modificaciones: en las clases Vehicle.java y TrafficSim.java

--- trunk/src/sim/app/TrafficSim.java
+++ trunk/src/sim/app/TrafficSim.java
@@ -14,134 +14,177 @@ import sim.app.geo.Road;
 import sim.app.geo.StreetXing;
 import sim.app.processing.Displayable;
 import sim.app.xml.XmlParseService;
+import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.Graph;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.util.HashMap;
 
 public abstract class TrafficSim {
 
-    public static int MAX_CAR_COUNT = 100;
-    protected Graph<StreetXing, Road> _city = new DirectedSparseGraph<StreetXing, Road>();
-    protected List<StreetXing> _sourceXings = new LinkedList<StreetXing>();
-    protected List<StreetXing> _destXings = new LinkedList<StreetXing>();
+	public static int MAX_CAR_COUNT = 3;
+	protected Graph<StreetXing, Road> _city = new DirectedSparseGraph<StreetXing, Road>();
+	protected List<StreetXing> _sourceXings = new LinkedList<StreetXing>();
+	protected List<StreetXing> _destXings = new LinkedList<StreetXing>();
+	protected HashMap<String, BufferedWriter> _hash = new HashMap<String, BufferedWriter>();
 
-    /**
-     * 
-     * @param cityDir
-     */
-    @Deprecated
-    protected void parseGraph(String cityDir) {
-	XmlParseService parsedGraph = new XmlParseService(System.getProperty("user.dir") + cityDir, getLogger());
-	_city = parsedGraph.getGraph();
-	MAX_CAR_COUNT = parsedGraph.getMaxCars();
-	_sourceXings = parsedGraph.getSourceXings();
-	_destXings = parsedGraph.getDestXings();
+	/**
+	 * 
+	 * @param cityDir
+	 */
+	@Deprecated
+	protected void parseGraph(String cityDir) {
+		XmlParseService parsedGraph = new XmlParseService(System
+				.getProperty("user.dir")
+				+ cityDir, getLogger());
+		_city = parsedGraph.getGraph();
+		MAX_CAR_COUNT = parsedGraph.getMaxCars();
+		_sourceXings = parsedGraph.getSourceXings();
+		_destXings = parsedGraph.getDestXings();
 
-    }
-
-    /**
-     * Display all displayable elements
-     * 
-     * @throws Exception
-     * 
-     */
-    public void display() {
-
-	for (Displayable disp : getRoads()) {
-	    disp.display();
-	}
-	// Display Agents
-	Iterator<Agent> iter = getAgents().iterator();
-	while (iter.hasNext()) {
-	    Agent agent = iter.next();
-	    if (agent instanceof DisplayableAgent) {
-		((DisplayableAgent) agent).display();
-	    }
-	}
-    }
-
-    /**
-     * Update Agents
-     */
-    public void update() {
-	Iterator<Agent> iter = getAgents().iterator();
-	while (iter.hasNext()) {
-	    Agent agent = iter.next();
-	    // If we're dealing with a car, remove it if it's dead.
-	    if (agent instanceof Vehicle && !((Vehicle) agent).isAlive()) {
-		iter.remove();
-	    } else {
-		agent.move();
-	    }
-	}
-    }
-
-    public abstract int getWidth();
-
-    public abstract int getHeight();
-
-    public abstract int getFrameRate();
-
-    /**
-     * 
-     * @return
-     */
-    protected StreetXing getSource() {
-	StreetXing pickedXing = null;
-	Random rand = new Random(System.currentTimeMillis());
-	int targetIndex = rand.nextInt(100);
-	int currentIndex = 0;
-
-	for (StreetXing xing : _sourceXings) {
-	    currentIndex += xing.getStartOdds();
-	    if (currentIndex >= targetIndex) {
-		pickedXing = xing;
-		break;
-	    }
-	}
-	return pickedXing;
-    }
-
-    /**
-     * Return a random car begining based on the {@code startingOdds} attribute
-     * in the city xml.
-     * <p/>
-     * See {@link TraffiSimulation.xsd} for more info
-     * 
-     * @return
-     */
-    protected StreetXing getDest() {
-	StreetXing pickedXing = null;
-	Random rand = new Random(System.currentTimeMillis());
-	int targetIndex = rand.nextInt(100);
-	int currentIndex = 0;
-
-	for (StreetXing xing : _destXings) {
-	    currentIndex += xing.getEndOdds();
-	    if (currentIndex >= targetIndex) {
-		pickedXing = xing;
-		break;
-	    }
 	}
 
-	return pickedXing;
-    }
+	/**
+	 * Display all displayable elements
+	 * 
+	 * @throws Exception
+	 * 
+	 */
+	public void display() {
 
-    protected abstract Logger getLogger();
+		for (Displayable disp : getRoads()) {
+			disp.display();
+		}
+		// Display Agents
+		Iterator<Agent> iter = getAgents().iterator();
+		while (iter.hasNext()) {
+			Agent agent = iter.next();
+			if (agent instanceof DisplayableAgent) {
+				((DisplayableAgent) agent).display();
+			}
+		}
+	}
 
-    protected abstract PApplet getApplet();
+	/**
+	 * Update Agents
+	 */
+	public void update() {
+		Iterator<Agent> iter = getAgents().iterator();
+		while (iter.hasNext()) {
+			Agent agent = iter.next();
+			// If we're dealing with a car, remove it if it's dead.
+			if (agent instanceof Vehicle && !((Vehicle) agent).isAlive()) {
+				Vehicle v = (Vehicle) agent;
+				String trayectory = v.getTrayectoryID();
 
-    /**
-     * Implement a time series to determine wether to create add a new vehicle
-     * in time T
-     */
-    protected abstract boolean addVhclP();
+				if (!_hash.containsKey(trayectory)) {
+					File folder = new File("Results");
+					if (!folder.exists()) {
+						folder.mkdir();
+					}
 
-    /**
-     * Create all the geo objs needed to describe the city
-     */
-    protected abstract void generateCity();
+					try {
+						BufferedWriter bw = new BufferedWriter(new FileWriter(
+								"Results//" + trayectory + ".txt", true));
+						_hash.put(trayectory, bw);
+					} catch (Exception ex) {
+						System.err.println("Error creating file: Results//"
+								+ trayectory + ".txt");
+						ex.printStackTrace();
+						System.exit(-1);
+					}
+				}
 
-    protected abstract List<Agent> getAgents();
+				try {
+					BufferedWriter bw = _hash.get(trayectory);
+					bw.write(v.getTravelDuration() + "");
+					bw.newLine();
+					bw.close();
+					System.out.println("Datos añadidos");
+				} catch (Exception ex) {
+					System.err.println("Error writting file Results//"
+							+ trayectory + ".txt");
+					ex.printStackTrace();
+					System.exit(-1);
+				}
 
-    protected abstract List<Road> getRoads();
+				// System.out.println
+				iter.remove();
+			} else {
+				agent.move();
+			}
+		}
+	}
+
+	public abstract int getWidth();
+
+	public abstract int getHeight();
+
+	public abstract int getFrameRate();
+
+	/**
+	 * 
+	 * @return
+	 */
+	protected StreetXing getSource() {
+		StreetXing pickedXing = null;
+		Random rand = new Random(System.currentTimeMillis());
+		int targetIndex = rand.nextInt(100);
+		int currentIndex = 0;
+
+		for (StreetXing xing : _sourceXings) {
+			currentIndex += xing.getStartOdds();
+			if (currentIndex >= targetIndex) {
+				pickedXing = xing;
+				break;
+			}
+		}
+		return pickedXing;
+	}
+
+	/**
+	 * Return a random car begining based on the {@code startingOdds} attribute
+	 * in the city xml.
+	 * <p/>
+	 * See {@link TraffiSimulation.xsd} for more info
+	 * 
+	 * @return
+	 */
+	protected StreetXing getDest() {
+		StreetXing pickedXing = null;
+		Random rand = new Random(System.currentTimeMillis());
+		int targetIndex = rand.nextInt(100);
+		int currentIndex = 0;
+
+		for (StreetXing xing : _destXings) {
+			currentIndex += xing.getEndOdds();
+			if (currentIndex >= targetIndex) {
+				pickedXing = xing;
+				break;
+			}
+		}
+
+		return pickedXing;
+	}
+
+	protected abstract Logger getLogger();
+
+	protected abstract PApplet getApplet();
+
+	/**
+	 * Implement a time series to determine wether to create add a new vehicle
+	 * in time T
+	 */
+	protected abstract boolean addVhclP();
+
+	/**
+	 * Create all the geo objs needed to describe the city
+	 */
+	protected abstract void generateCity();
+
+	protected abstract List<Agent> getAgents();
+
+	protected abstract List<Road> getRoads();
 }
--- trunk/src/sim/app/agents/display/vehicle/Vehicle.java
+++ trunk/src/sim/app/agents/display/vehicle/Vehicle.java
@@ -7,7 +7,10 @@ import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//import com.sun.tools.javac.resources.compiler;
 
 import processing.core.PApplet;
 import sim.app.agents.display.DisplayableAgent;
@@ -25,292 +28,376 @@ import edu.uci.ics.jung.graph.Graph;
  */
 public abstract class Vehicle extends DisplayableAgent {
 
-    private static int _vhclCount = 0;
-    private static int _idToken = 0; // ID token always grows== ensure id
-    // uniqueness.
-    private static Logger _log;
-    private static Graph<StreetXing, Road> _city;
+	private static int _vhclCount = 0;
+	private static int _idToken = 0; // ID token always grows== ensure id
+	// uniqueness.
+	private static Logger _log;
+	private static Graph<StreetXing, Road> _city;
 
-    private final String ID;
-    private Point2D _currLocation; // How close to next intersection
-    private Line2D _currRoadLine;
-    private Distance _currSpeed;
-    private long _time;
-    private LinkedList<Road> _trayectory;
-    private boolean _isAlive;
-    private int _roadLineIdx = 0;
-    private int _subRoadIdx = 0;
+	private final String ID;
+	private Point2D _currLocation; // How close to next intersection
+	private Line2D _currRoadLine;
+	private Distance _currSpeed;
+	private long _time;
+	private LinkedList<Road> _trayectoryList;
+	private boolean _isAlive;
+	private int _roadLineIdx = 0;
+	private int _subRoadIdx = 0;
 
-    /**
-     * Given in K. Assumed to be K/s
-     */
-    public abstract Distance getMaxVelocity();
+	//ANTONIO
+	private String _trayectoryID="";
+	private long _initialTime;
+	private long _travelTime=0;
 
-    /**
-     * Given in K. Assumed to be K/s^2
-     */
-    public abstract Distance getAcceleration();
+	/**
+	 * Given in K. Assumed to be K/s
+	 */
+	public abstract Distance getMaxVelocity();
 
-    public int getRoadLineIdx() {
-	return _roadLineIdx;
-    }
+	/**
+	 * Given in K. Assumed to be K/s^2
+	 */
+	public abstract Distance getAcceleration();
 
-    /**
-     * Given in m.
-     */
-    public abstract Distance getSize();
+	public int getRoadLineIdx() {
+		return _roadLineIdx;
+	}
 
-    public Point2D getLocation() {
-	return _currLocation;
-    }
+	/**
+	 * Given in m.
+	 */
+	public abstract Distance getSize();
 
-    public Line2D getRoadLine() {
-	return _currRoadLine;
-    }
+	public Point2D getLocation() {
+		return _currLocation;
+	}
 
-    /**
-     * Class constructor.
-     * 
-     * @param trayectory_
-     */
-    public Vehicle(List<Road> trayectory_, Graph<StreetXing, Road> city_, Logger log_, PApplet parent_) {
-	super.applet = parent_;
-	_isAlive = true;
-	ID = "Car_" + _idToken + "_" + _vhclCount;
-	_city = city_;
-	_vhclCount++;
-	_idToken++;
-	_trayectory = new LinkedList<Road>(trayectory_);
-	updateRoad();
-	_currSpeed = new Kilometers(0.0);
-	_time = System.currentTimeMillis();
-	_log = log_;
-	// Add car to the streetQue
-	currentRoad().getVehiclesOnRoad().add(this);
-	// _log.log( Level., "Created: " + this );
-    }
+	public Line2D getRoadLine() {
+		return _currRoadLine;
+	}
 
-    /**
-     * Displays the vehicle according to its location variables. Gets called
-     * once per frame
-     */
-    public void display() {
-	super.applet.stroke(0, 0, 255);
-	super.applet.strokeWeight(3);
-	super.applet.point((float) _currLocation.getX(), (float) _currLocation.getY());
-    }
-
-    /**
-     * Updates the location variables of this vehicle. Gets called once per
-     * frame.
-     * 
-     * @throws Exception
-     */
-    public void move() {
-
-	if (!_trayectory.isEmpty()) {
-	    // does next xing have a tf?
-	    if (canMove()) {
-		moveVehicle();
-	    } else {
-		// Car stops. Reset time and speed.
+	/**
+	 * Class constructor.
+	 * 
+	 * @param trayectory_
+	 */
+	public Vehicle(List<Road> trayectory_, Graph<StreetXing, Road> city_, Logger log_, PApplet parent_) {
+		super.applet = parent_;
+		_isAlive = true;
+		ID = "Car_" + _idToken + "_" + _vhclCount;
+		_city = city_;
+		_vhclCount++;
+		_idToken++;
+		_trayectoryList = new LinkedList<Road>(trayectory_);
+		updateRoad();
+		_currSpeed = new Kilometers(0.0);
 		_time = System.currentTimeMillis();
-		_currSpeed.setVal(0.0);
-	    }
-	    _time++;
-	} else {
-	    die();
+		_log = log_;
+		// Add car to the streetQue
+		currentRoad().getVehiclesOnRoad().add(this);
+		// _log.log( Level., "Created: " + this );
+
+		//ANTONIO
+		_initialTime = System.currentTimeMillis();
+//		for(Road r : trayectory_){
+//			_trayectoryID += r.ID + "_";
+//		}
 	}
-    }
 
-    public boolean isAlive() {
-	return _isAlive;
-    }
+	/**
+	 * Displays the vehicle according to its location variables. Gets called
+	 * once per frame
+	 */
+	public void display() {
+		super.applet.stroke(0, 0, 255);
+		super.applet.strokeWeight(3);
+		super.applet.point((float) _currLocation.getX(), (float) _currLocation.getY());
+	}
 
-    /**
-     * Delete the car from schedule loop.
-     * 
-     * @param state
-     */
-    // public void die ( final SimState state )
-    // {
-    // _log.log( Level.INFO, this + "Dying..." );
-    // _carCount--;
-    //        
-    // if ( toDiePointer != null )
-    // toDiePointer.stop();
-    // }
+	/**
+	 * Updates the location variables of this vehicle. Gets called once per
+	 * frame.
+	 * 
+	 * @throws Exception
+	 */
+	public void move() {
 
-    /**
-     * Returns number of cars currently in play
-     * 
-     * @return
-     */
-    public static int getNumberOfCars() {
-	return _vhclCount;
-    }
-
-    /**
-     * Overrite toString to print car id: car_[number in simulation]_[number in
-     * city < MAX_CAR ]
-     */
-    public String toString() {
-	return ID;
-    }
-
-
-    /**
-     * Returns true if car in front is not too close.
-     */
-    protected boolean canMove() {
-	boolean canMove = false;
-	    boolean isSaturated = false;
-
-	if (!atLastSeg()) {
-
-	    int nextSegIdx = currentRoad().getSegIdx(_currRoadLine, _currLocation) + 1;
-	    if (nextSegIdx < currentRoad().getSegmentList().get(_roadLineIdx).size()) {
-		Line2D nextSeg = currentRoad().getSegmentList().get(_roadLineIdx).get(nextSegIdx);
-		
-		isSaturated = currentRoad().isSegSaturated(nextSeg);		
-	    } else if ((_roadLineIdx + 1) < currentRoad().getSegmentList().size()) {
-		Line2D nextSeg = currentRoad().getSegmentList().get(_roadLineIdx + 1).get(0);
-		isSaturated = currentRoad().isSegSaturated(nextSeg);
-	    }
-
-	    canMove = !isSaturated;
-	} else {
-	    if(_trayectory.size() > 1)
-	    {
-		Line2D nextSeg = _trayectory.get(1).getSegmentList().get(0).get(0);
-		isSaturated = _trayectory.get(1).isSegSaturated(nextSeg);
-	    }
-	    if (currentXing().hasTrafficLight()) {
-		Point2D tfLoc = currentXing().getLocation();
-		Meters distance = new Meters(getLocation().distance(tfLoc));
-		if (distance.toMeters() <= Road.LAYER_SEG.toMeters())		    
-		{
-		    TrafficLightState tfState = currentRoad().getTf();
-		    canMove = TrafficLightState.RED != tfState && !isSaturated;
+		if (!_trayectoryList.isEmpty()) {
+			// does next xing have a tf?
+			if (canMove()) {
+				moveVehicle();
+			} else {
+				// Car stops. Reset time and speed.
+				_time = System.currentTimeMillis();
+				_currSpeed.setVal(0.0);
+			}
+			_time++;
+		} else {
+			die();
 		}
-
-	    }else
-	    {
-		canMove = true;
-	    }
 	}
-	return canMove;
-    }
 
-    /**
-     * Resets locations variables when a new road is reached.
-     */
-    private void updateRoad() {
-	_currLocation = currentRoad().startLoc();
-	_roadLineIdx = 0;
-	_currRoadLine = currentRoad().getLineList().get(_roadLineIdx);
-    }
-
-    /**
-     * Add this car to the current street it's cruising
-     * 
-     * @param car
-     */
-    private void addtoCurrRoad() {
-
-	currentRoad().getVehiclesOnRoad().add(this);
-	updateRoad();
-    }
-
-    /**
-     * Wrapper method to remove this car from the street queue
-     */
-    private void removeFromCurrRoad() {
-	currentRoad().removeVFromRoad(this);
-//	_trayectory.getFirst().getVehiclesOnRoad().remove(this);
-    }
-
-    /**
-     * Move car from one street to the next
-     */
-    private void goToNextRoad() {
-	removeFromCurrRoad();
-	_trayectory.pop();
-	if (!_trayectory.isEmpty()) {
-	    addtoCurrRoad();
+	public boolean isAlive() {
+		return _isAlive;
 	}
-    }
 
-    /**
-     * TODO Calculate new Speed
-     */
-    private void accelerate() {
+	/**
+	 * Delete the car from schedule loop.
+	 * 
+	 * @param state
+	 */
+	// public void die ( final SimState state )
+	// {
+	// _log.log( Level.INFO, this + "Dying..." );
+	// _carCount--;
+	//        
+	// if ( toDiePointer != null )
+	// toDiePointer.stop();
+	// }
+
+	/**
+	 * Returns number of cars currently in play
+	 * 
+	 * @return
+	 */
+	public static int getNumberOfCars() {
+		return _vhclCount;
+	}
+
+	/**
+	 * Overrite toString to print car id: car_[number in simulation]_[number in
+	 * city < MAX_CAR ]
+	 */
+	public String toString() {
+		return ID;
+	}
+
+	/**
+	 * Delete the car from schedule loop.
+	 * 
+	 * @param state
+	 */
+	// public void die ( final SimState state )
+	// {
+	// _log.log( Level.INFO, this + "Dying..." );
+	// _carCount--;
+	//        
+	// if ( toDiePointer != null )
+	// toDiePointer.stop();
+	// }
+
+	/**
+	 * Returns true if car in front is not too close.
+	 */
+	protected boolean canMove() {
+		boolean canMove = false;
+		boolean isSaturated = false;
+
+		if (!atLastSeg()) {
+
+			int nextSegIdx = currentRoad().getSegIdx(_currRoadLine, _currLocation) + 1;
+			if (nextSegIdx < currentRoad().getSegmentList().get(_roadLineIdx).size()) {
+				Line2D nextSeg = currentRoad().getSegmentList().get(_roadLineIdx).get(nextSegIdx);
+
+				isSaturated = currentRoad().isSegSaturated(nextSeg);		
+			} else if ((_roadLineIdx + 1) < currentRoad().getSegmentList().size()) {
+				Line2D nextSeg = currentRoad().getSegmentList().get(_roadLineIdx + 1).get(0);
+				isSaturated = currentRoad().isSegSaturated(nextSeg);
+			}
+
+			canMove = !isSaturated;
+		} else {
+			if(_trayectoryList.size() > 1)
+			{
+				Line2D nextSeg = _trayectoryList.get(1).getSegmentList().get(0).get(0);
+				isSaturated = _trayectoryList.get(1).isSegSaturated(nextSeg);
+			}
+			if (currentXing().hasTrafficLight()) {
+				Point2D tfLoc = currentXing().getLocation();
+				Meters distance = new Meters(getLocation().distance(tfLoc));
+				if (distance.toMeters() <= Road.LAYER_SEG.toMeters())		    
+				{
+					TrafficLightState tfState = currentRoad().getTf();
+					canMove = TrafficLightState.RED != tfState && !isSaturated;
+				}
+
+			}else
+			{
+				canMove = true;
+			}
+		}
+		return canMove;
+	}
+
+	/**
+	 * Resets locations variables when a new road is reached.
+	 */
+	private void updateRoad() {
+		_currLocation = currentRoad().startLoc();
+		updateRouteHistory();
+		_roadLineIdx = 0;
+		_currRoadLine = currentRoad().getLineList().get(_roadLineIdx);	
+	}
+
+	//ANTONIO
+	private void updateRouteHistory(){
+		String roadID = String.valueOf(currentRoad().ID);
+		if(!_trayectoryID.endsWith(roadID)){
+			_trayectoryID += "_" + roadID;
+		}
+	}
+	
+	public String getTrayectoryID(){
+		return _trayectoryID;
+	}
+
+	public long getTravelDuration(){
+		return _travelTime;
+	}
+
+	private void updateTrarvelTime(){
+		long currentTime = System.currentTimeMillis();
+		_travelTime = currentTime - _initialTime;
+	}
+
+	/**
+	 * Add this car to the current street it's cruising
+	 * 
+	 * @param car
+	 */
+	private void addtoCurrRoad() {
+
+		currentRoad().getVehiclesOnRoad().add(this);
+		updateRoad();
+	}
+
+	/**
+	 * Wrapper method to remove this car from the street queue
+	 */
+	private void removeFromCurrRoad() {
+		currentRoad().removeVFromRoad(this);
+		//	_trayectory.getFirst().getVehiclesOnRoad().remove(this);
+	}
+
+	/**
+	 * Move car from one street to the next
+	 */
+	private void goToNextRoad() {
+		removeFromCurrRoad();
+		_trayectoryList.pop();
+		if (!_trayectoryList.isEmpty()) {
+			addtoCurrRoad();
+		}
+	}
+
+	/**
+	 * TODO Calculate new Speed
+	 */
+	/* private void accelerate() {
 	if (_currSpeed.toMeters() < currentRoad().getMaxVelocity().toMeters()
 		&& _currSpeed.toMeters() < getMaxVelocity().toMeters()) {
 	    _currSpeed.setVal(getAcceleration().getVal() * _time);
 	}
 
-    }
+    }*/
 
-    /**
-     * @param state_
-     * @return
-     */
-    private StreetXing currentXing() {
-	return _city.getDest((Street) currentRoad());
-    }
+	/**
+	 * Delete the car from schedule loop.
+	 * 
+	 * @param state
+	 */
+	// public void die ( final SimState state )
+	// {
+	// _log.log( Level.INFO, this + "Dying..." );
+	// _carCount--;
+	//        
+	// if ( toDiePointer != null )
+	// toDiePointer.stop();
+	// }
 
-    /**
-     * 
-     * @return current street
-     */
-    private Road currentRoad() {
-	return _trayectory.getFirst();
-    }
+	/**
+	 * @param state_
+	 * @return
+	 */
+	private StreetXing currentXing() {
+		return _city.getDest((Street) currentRoad());
+	}
 
+	/**
+	 * 
+	 * @return current street
+	 */
+	private Road currentRoad() {
+		return _trayectoryList.getFirst();
+	}
 
-    /**
-     * 
-     * @return true if closer to the destination than the Distance threshold
-     */
-    private boolean atLastSeg() {
-	Point2D dest = _city.getDest(currentRoad()).getLocation();
+	/**
+	 * 
+	 * @return true if closer to the destination than the Distance threshold
+	 */
+	private boolean atEndOfRoad() {
+		Point2D dest = _city.getDest(currentRoad()).getLocation();
+		return (_currLocation.distance(dest) <= Road.DISTANCE_THRESHOLD);
+	}
 
-	return (_currLocation.distance(dest) <= Road.LAYER_SEG.toMeters());
-    }
+	/**
+	 * 
+	 * @return true if closer to the destination than the Distance threshold
+	 */
+	private boolean atLastSeg() {
+		Point2D dest = _city.getDest(currentRoad()).getLocation();
 
-    /**
-     * @throws Exception
-     * 
-     */
-    private void moveVehicle() {
-	    Point2D newLoc = currentRoad().getNewLocation(_currLocation, 1);
-	    if (_currLocation != newLoc) {
-		_currLocation = newLoc;
-		if (currentRoad().getLineList().size() > _roadLineIdx + 1) {
-		    Line2D nextLine = currentRoad().getLineList().get(_roadLineIdx + 1);
-		    int compA = Double.compare(_currRoadLine.ptLineDist(_currLocation), Road.DISTANCE_THRESHOLD);
-		    int compB = Double.compare(nextLine.ptLineDist(_currLocation), Road.DISTANCE_THRESHOLD);
-		    if (compA >= compB) {
-			updateRoadSeg();
-		    }
-		}
-		currentRoad().updateVehicle(this);
+		return (_currLocation.distance(dest) <= Road.LAYER_SEG.toMeters());
+	}
 
-	    } else if(currentRoad().getLineList().size() > _subRoadIdx){
-		goToNextRoad();
-	    }
-    }
-
-    /**
+	/**
+	 * @throws Exception
 	 * 
 	 */
-    private void updateRoadSeg() {
-	_roadLineIdx++;
-	_currRoadLine = currentRoad().getLine(_roadLineIdx);
-    }
+	private void moveVehicle() {
+		//	if (!atEndOfRoad()) {
+		Point2D newLoc = currentRoad().getNewLocation(_currLocation, 1);
+		if (_currLocation != newLoc) {
+			_currLocation = newLoc;
+			updateRouteHistory();
+			if (currentRoad().getLineList().size() > _roadLineIdx + 1) {
+				Line2D nextLine = currentRoad().getLineList().get(_roadLineIdx + 1);
+				int compA = Double.compare(_currRoadLine.ptLineDist(_currLocation), Road.DISTANCE_THRESHOLD);
+				int compB = Double.compare(nextLine.ptLineDist(_currLocation), Road.DISTANCE_THRESHOLD);
+				if (compA >= compB) {
+					updateRoadSeg();
+				}
+			}//else if(currentRoad().getSubRoadList().size() > _subRoadIdx)
+			//		{
+			//		    updateSubRoad();
+			//		}
+			currentRoad().updateVehicle(this);
 
-    private void die() {
-	_isAlive = false;
-    }
+		} else if(currentRoad().getLineList().size() > _subRoadIdx){
+			goToNextRoad();
+		}
+		//	} else {
+		//	    goToNextRoad();
+		//	}
+	}
+
+	private void updateSubRoad() {
+		_subRoadIdx++;
+		_roadLineIdx = 0;
+		_currRoadLine = currentRoad().getSubRoad(_subRoadIdx).getLine(0);
+	}
+
+	/**
+	 * 
+	 */
+	private void updateRoadSeg() {
+		_roadLineIdx++;
+		_currRoadLine = currentRoad().getLine(_roadLineIdx);
+	}
+
+	private void die() {
+		//	currentRoad().removeVFromRoad(this);
+		_isAlive = false;
+		updateTrarvelTime();
+	}
 }
