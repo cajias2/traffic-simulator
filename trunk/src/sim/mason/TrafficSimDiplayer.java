6961865a6e3fe906b8aec9818a788f8887c23a24
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2010-06-26 10:50:30 +0000

    Moving things around. Not Stable

---
+++ trunk/src/sim/mason/TrafficSimDiplayer.java
@@ -0,0 +1,44 @@
+package sim.app.processing;
+
+import java.util.logging.Logger;
+
+import processing.core.PApplet;
+import sim.app.TrafficSim;
+
+@SuppressWarnings("serial")
+public abstract class TrafficSimDiplayer extends CitySimState {
+
+    private static TrafficSim _sim;
+    private Logger _log;
+    private long _duration;
+
+    /**
+     * Gets called first by PApplet. Creates a new simulation.
+     */
+    public void setup() {
+	_duration = System.currentTimeMillis();
+	_log = Logger.getLogger(sketchPath);
+	_log.info("Simulation staterd\n");
+	_sim = getNewSimulation(this, _log);
+	size(_sim.getWidth(), _sim.getHeight());
+	frameRate(_sim.getFrameRate());
+    }
+
+    /**
+     * Called by PApplet. Draws the simulation.
+     */
+    public void draw() {
+	background(100);
+	if (_sim.getSimDuration() >= this.frameCount) {
+	    _sim.display();
+	    _sim.update();
+	} else {
+	    _sim.end();
+	    _log.info("Simulation ended. Took: " + (System.currentTimeMillis() - _duration) / 1000 + "sec");
+	    this.exit();
+	}
+    }
+    
+
+    protected abstract TrafficSim getNewSimulation(PApplet applet_, Logger log);
+}
