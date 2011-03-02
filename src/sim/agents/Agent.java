3ca61921f1726b9c333b5d82985a7da7cd7a25a1
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2011-03-02 14:35:22 +0000

    Icsoft changes. Adding social code. Not Stable.

--- src/sim/agents/Agent.java
+++ src/sim/agents/Agent.java
@@ -4,12 +4,13 @@
 package sim.agents;
 
 import sim.engine.SimState;
+import sim.engine.Steppable;
 
 /**
  * @author biggie
  * 
  */
-public abstract class Agent {
+public abstract class Agent extends Entity implements Steppable {
 
     /**
      * Update the state of this agent.
