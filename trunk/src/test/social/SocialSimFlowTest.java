1fdd27f214becada99afcde210b82a99984a40f4
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2011-05-17 08:19:46 +0000

    refactoring: Unify all sources in one src/ folder

--- trunk/src/test/social/SocialSimFlowTest.java
+++ trunk/src/test/social/SocialSimFlowTest.java
@@ -3,27 +3,24 @@
  * @author biggie
  * 
  */
-
-package test.social;
+    
+    package test.social;
 
 import java.awt.Dimension;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 import javax.swing.JFrame;
 
-import org.jgrapht.UndirectedGraph;
-
+import sim.app.social.SocialSim;
 import sim.field.network.Edge;
 import sim.graph.social.link.FriendLink;
 import sim.graph.social.metrics.BronKerboschKCliqueFinder;
 import sim.graph.social.metrics.CPMCommunityFinder;
 import sim.graph.social.metrics.commTracker.TimeLineList;
 import sim.mason.AgentNetwork;
-import sim.app.social.SocialSim;
 import edu.uci.ics.jung.algorithms.layout.HypergraphLayout;
 import edu.uci.ics.jung.algorithms.layout.SpringLayout2;
 import edu.uci.ics.jung.graph.Graph;
@@ -32,11 +29,11 @@ import edu.uci.ics.jung.graph.SetHypergraph;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.renderers.BasicHypergraphRenderer;
 
-/**
- * TODO Purpose
- * 
- * @author biggie
- */
+    /**
+     * TODO Purpose
+     * 
+     * @author biggie
+     */
 public class SocialSimFlowTest {
 
     /**
@@ -48,46 +45,40 @@ public class SocialSimFlowTest {
      */
     public static void main(String[] args) {
 	SocialSim sim = new SocialSim(System.currentTimeMillis());
-	//El 100 es el snapshot
-	List<Edge[][]> adjList = sim.runSim(args, 50);
+	// El 100 es el snapshot
+	List<Edge[][]> adjList = sim.runSim(args, 1);
 	System.out.println("Finding Communities....");
 	System.out.println("Cliques\tCommunities");
 	List<Collection<Set<Integer>>> kEvol = new LinkedList<Collection<Set<Integer>>>();
 	List<Graph<Integer, FriendLink>> graphEvol = new LinkedList<Graph<Integer, FriendLink>>();
 	int lastKsize = -1;
-	
+
 	TimeLineList evolution = new TimeLineList();
 	int snapshot = 1;
-	
+
 	for (Edge[][] eAr : adjList) {
-		
-		Graph<Integer, FriendLink> graph = AgentNetwork.adjListToJungGraph(eAr);
-        BronKerboschKCliqueFinder<Integer, FriendLink> maxCliques = new BronKerboschKCliqueFinder<Integer, FriendLink>(
-                graph);
-        Collection<Set<Integer>> kFourCliques = maxCliques.getAllMaxKCliques(3);
 
-        // Conjunto de communidades en ese snapshot
-        Collection<Set<Integer>> kComs = findCPM(kFourCliques);
-                        
-        kEvol.add(kComs);
-        graphEvol.add(AgentNetwork.adjListToJungGraph(eAr));
+	    Graph<Integer, FriendLink> graph = AgentNetwork.adjListToJungGraph(eAr);
+	    BronKerboschKCliqueFinder<Integer, FriendLink> maxCliques = new BronKerboschKCliqueFinder<Integer, FriendLink>(
+		    graph);
+	    Collection<Set<Integer>> kFourCliques = maxCliques.getAllMaxKCliques(3);
 
-//			System.out.println(kFourCliques.size() + "\t" + kComs.size());
-			Graph<Integer, FriendLink> g = AgentNetwork.adjListToJungGraph(eAr);
+	    // Conjunto de communidades en ese snapshot
+	    Collection<Set<Integer>> kComs = findCPM(kFourCliques);
 
-			for (Set<Integer> community : kComs) {
-				evolution.set(snapshot, community, g);
-			}
-			snapshot++;
-	    
-	    if (-1 == lastKsize) {
-		lastKsize = kComs.size();
-	    } else if (1 == kComs.size() && kComs.size() < lastKsize) {
-		break;
+	    kEvol.add(kComs);
+	    graphEvol.add(AgentNetwork.adjListToJungGraph(eAr));
+
+	    // System.out.println(kFourCliques.size() + "\t" + kComs.size());
+	    Graph<Integer, FriendLink> g = AgentNetwork.adjListToJungGraph(eAr);
+
+	    for (Set<Integer> community : kComs) {
+		evolution.set(snapshot, community, g);
 	    }
-	    lastKsize = kComs.size();
+	    snapshot++;
+
 	}
-	
+
 	evolution.writeMetrics();
 
 	JFrame frame = new JFrame("Simple Graph View");
@@ -96,14 +87,12 @@ public class SocialSimFlowTest {
 	Hypergraph<Integer, Integer> hg = populateHg(graphEvol.get(1), kEvol.get(1));
 	HypergraphLayout<Integer, Integer> l = new HypergraphLayout<Integer, Integer>(hg, SpringLayout2.class);
 
-	VisualizationViewer<Integer, Integer> v = new VisualizationViewer<Integer, Integer>(l, new Dimension(600,
-		600));
-	
+	VisualizationViewer<Integer, Integer> v = new VisualizationViewer<Integer, Integer>(l, new Dimension(600, 600));
+
 	v.setRenderer(new BasicHypergraphRenderer<Integer, Integer>());
 	frame.getContentPane().add(v);
 	frame.pack();
 	frame.setVisible(true);
-
 
     }
 
@@ -120,14 +109,14 @@ public class SocialSimFlowTest {
      */
     private static <V, E> Hypergraph<V, Integer> populateHg(Graph<V, E> graph_, Collection<Set<V>> collection_) {
 	Hypergraph<V, Integer> hg = new SetHypergraph<V, Integer>();
-	for(V node: graph_.getVertices()){
+	for (V node : graph_.getVertices()) {
 	    hg.addVertex(node);
 	}
 	int cliqueCount = 0;
-	for(Set<V> kclique : collection_){
+	for (Set<V> kclique : collection_) {
 	    hg.addEdge(cliqueCount++, kclique);
 	}
-	
+
 	return hg;
 
     }
@@ -144,5 +133,5 @@ public class SocialSimFlowTest {
 	CPMCommunityFinder<Integer> cpm = new CPMCommunityFinder<Integer>(kcliques_);
 	return cpm.findCommunities(4);
     }
-    
-}
+
+    }
