0cc32c795dae5ee22daccf04cda2e7ff5a3ab6b0
raul.cajias <raul.cajias@3ff1e900-ddd1-11de-82ea-1f113a1b7651>
2011-11-12 17:24:06 +0000

    Refactoring of last checking

--- src/sim/agents/social/DBWriterAgent.java
+++ src/sim/agents/social/DBWriterAgent.java
@@ -1,48 +1,58 @@
 package sim.agents.social;
 
+import sim.agents.Agent;
+import sim.app.social.SocialSim;
 import sim.engine.SimState;
 import sim.engine.Steppable;
 import sim.graph.utils.Edge;
-import sim.agents.Agent;
-import sim.app.social.SocialSim;
-import sim.app.social.db.DBManager;
 import edu.uci.ics.jung.graph.Graph;
 
 public class DBWriterAgent implements Steppable {
 
-	private DBManager _dbConnection;
-	
-	public DBWriterAgent() {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4420057740757549336L;
+
+    public DBWriterAgent() {
+    }
+
+    /**
+     * 
+     */
+    public void step(SimState state_) {
+	@SuppressWarnings("unchecked")
+	SocialSim<Agent, String> socSim = (SocialSim<Agent, String>) state_;
+	writeDeltaToDB(socSim);
+    }
+
+    /**
+     * @param socSim
+     */
+    private void writeDeltaToDB(SocialSim<Agent, String> socSim) {
+	Graph<Agent, Edge> graphChanges = socSim._temporalNetwork;
+	int id = socSim.getSimID();
+	int step = (int) socSim.schedule.getSteps();
+
+	int edges = graphChanges.getEdgeCount();
+	if (edges > 0) {
+
+	    boolean changes = false;
+	    for (Edge e : graphChanges.getEdges()) {
+		int from = e.getSource();
+		int to = e.getDest();
+		boolean createEdge = e.addEdge();
+
+		socSim.getDBManager().addEdge(id, step, from, to, createEdge);
+
+		changes = true;
 	    }
-	
-	public void step(SimState state_) {
-		SocialSim<Agent, String> socSim = (SocialSim<Agent, String>) state_;
-		_dbConnection = socSim.getDBManager();
-		Graph<Agent, Edge> graphChanges = socSim._temporalNetwork;
-		int id = _dbConnection.getSimID();
-		int step = (int)socSim.schedule.getSteps();
-		
-		int edges = graphChanges.getEdgeCount(); 
-		if(edges>0){
 
-		boolean changes = false;
-		for(Edge e : graphChanges.getEdges()){
-			int from = e.getSource();
-			int to = e.getDest();
-			boolean createEdge = e.addEdge();
-							
-			_dbConnection.addEdge(id, step, from, to, createEdge);
-			
-			changes = true;
-		}
-		
-		if(changes){	
-			_dbConnection.insertEdges();
-		}	
-		
-		}
-		
-		socSim.resetTemporalNetwork();
+	    if (changes) {
+		socSim.getDBManager().insertEdges();
+	    }
 	}
-
+	socSim.resetTemporalNetwork();
+    }
 }
